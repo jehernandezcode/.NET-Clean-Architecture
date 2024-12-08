aa7579f86ef3c3e675c5cf922008efd2750d3598
jehernandezcode <jhonnhernandez754@gmail.com>
2024-12-08 11:00:35 -0500

    feat: layer application add reflexion AssemblyReference and abstraction Db Context for customer

---
+++ EasyPos/Application/ApplicationAssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Reflection;
+
+namespace Application
+{
+    public class ApplicationAssemblyReference
+    {
+        internal static readonly Assembly assembly = typeof(ApplicationAssemblyReference).Assembly;
+    }
+}
