409349be2a16ea403c0aeb1d75733477db20e3a8
jehernandezcode <jhonnhernandez754@gmail.com>
2024-12-11 20:16:22 -0500

    feat: generate and run migrations. inyection of dependencies by layers of ddd

---
+++ EasyPos/Web.API/DependencyInyection.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Web.API
+{
+    public static class DependencyInyection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
--- EasyPos/Web.API/Program.cs
+++ EasyPos/Web.API/Program.cs
@@ -1,9 +1,15 @@
+using Application;
+using Infraestructure;
+using Web.API;
+using Web.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddPresentation()
+    .AddInfraestructure(builder.Configuration)
+    .AddApplication();
 
 var app = builder.Build();
 
@@ -12,6 +18,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
