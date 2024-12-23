5ce880535e90e38f6830152447480638d468ff6c
jehernandezcode <jhonnhernandez754@gmail.com>
2024-12-23 16:19:33 -0500

    refactor: test protection rule CI

--- src/Domain/DomainErrors/ErrorsCustomer.cs
+++ src/Domain/DomainErrors/ErrorsCustomer.cs
@@ -6,7 +6,7 @@ public static partial class CustomErrors
 {
     public static class Customer
     {
-        public static Error PhoneNumberWithBabFormat => Error.Validation("Customer.PhoneNumber", "Phone number is not valid format");
+        public static Error PhoneNumberWithBabFormat => Error.Validation("Customer.PhoneNumber", "Phone  number is not valid format");
 		public static Error AddressWithFormatInvalid => Error.Validation("Customer.Address", "Address is not valid");
 	}
 }
