1121557f9a1a88042e12fe416bf8f8958c096a3c
jehernandezcode <jhonnhernandez754@gmail.com>
2024-12-23 15:10:01 -0500

    feat: :test: add unit test by command create customer error case

--- test/UnitTests/Application.Customers.UnitTests/Create/CreateCustomerCommandHandlerUnitTests.cs
+++ test/UnitTests/Application.Customers.UnitTests/Create/CreateCustomerCommandHandlerUnitTests.cs
@@ -1,7 +1,10 @@
 ﻿
 using Application.Customers.Create;
 using Domain.Customers;
+using Domain.DomainErrors;
 using Domain.Primitives;
+using ErrorOr;
+using FluentAssertions;
 using Moq;
 
 namespace Application.Customers.UnitTests.Create;
@@ -19,8 +22,24 @@ public class CreateCustomerCommandHandlerUnitTests
     }
 
     [Fact]
-    public void HandleCreateCustomer_WhenPhoneNumberHasBadFormat_ShouldReturnValidationError()
+    public async Task HandleCreateCustomer_WhenPhoneNumberHasBadFormat_ShouldReturnValidationError()
     {
+        CreateCustomerCommand command = new CreateCustomerCommand("JHonn","Vega", "jhonnn@sdfsdf.com", "+57 12345678901", "col", "line 1","line 2","Paez", "Boyaca","15003");
+        var result = await _handler.Handle(command, default);
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorType.Validation);
+        result.FirstError.Code.Should().Be(CustomErrors.Customer.PhoneNumberWithBabFormat.Code);
+        result.FirstError.Description.Should().Be(CustomErrors.Customer.PhoneNumberWithBabFormat.Description);
+    }
 
+    [Fact]
+    public async Task HandleCreateCustomer_WhenAddressHasBadFormat_ShouldReturnValidationError()
+    {
+        CreateCustomerCommand command = new CreateCustomerCommand("JHonn", "Vega", "jhonnn@sdfsdf.com", "+57 1234567890", "col", "", "line 2", "Paez", "Boyaca", "15003");
+        var result = await _handler.Handle(command, default);
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorType.Validation);
+        result.FirstError.Code.Should().Be(CustomErrors.Customer.AddressWithFormatInvalid.Code);
+        result.FirstError.Description.Should().Be(CustomErrors.Customer.AddressWithFormatInvalid.Description);
     }
 }
